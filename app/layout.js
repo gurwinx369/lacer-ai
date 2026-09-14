7674689351ccfdb9f184e699e7a19d87adad8f71
Gurwinder Singh <gurwinx369@gmail.com>
2026-09-14 13:53:52 +0530

    feat: Initialized repo

--- app/layout.js
+++ app/layout.js
@@ -18,11 +18,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
+    <html suppressHydrationWarning lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body suppressHydrationWarning className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
+
+
